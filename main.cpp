8bcf249673df084df739763e1c318ed168829881
bharat2002 <bpaliwal70@gmail.com>
2025-09-08 01:57:19 +0530

    Updated Iterators concept for Singly Linked List

--- main.cpp
+++ main.cpp
@@ -5,27 +5,40 @@ using namespace std;
 int main() {
     LinkedList<int> list;
     list.addFirst(10);
-    LinkedListPrinter<int>::printList(list);
-    LinkedListPrinter<int>::printList(list);
+
+    // delete iterator;
+    // LinkedListPrinter<int>::printList(list);
+    // LinkedListPrinter<int>::printList(list);
     list.addFirst(20);
-    LinkedListPrinter<int>::printList(list);
+    // LinkedListPrinter<int>::printList(list);
     list.addFirst(30);
-    LinkedListPrinter<int>::printList(list);
+    // LinkedListPrinter<int>::printList(list);
     list.addFirst(40);
-    LinkedListPrinter<int>::printList(list);
+    auto iterator = list.createIterator();
+     while(iterator->hasNext()) {
+        cout << iterator->next() << " ";
+    }
+    cout << endl;
+    delete iterator;
+    // LinkedListPrinter<int>::printList(list);
     list.addFirst(50);
-    LinkedListPrinter<int>::printList(list);
+    // LinkedListPrinter<int>::printList(list);
     list.addAt(2, 25);
-    LinkedListPrinter<int>::printList(list);
+    // LinkedListPrinter<int>::printList(list);
     list.addLast(5);
-    
-    LinkedListPrinter<int>::printList(list);
+    iterator = list.createIterator();
+     while(iterator->hasNext()) {
+        cout << iterator->next() << " ";
+    }
+    cout << endl;
+    delete iterator;
+    // LinkedListPrinter<int>::printList(list);
     list.addLast(1);
-    LinkedListPrinter<int>::printList(list);
+    // LinkedListPrinter<int>::printList(list);
     list.deleteFirst();
-    LinkedListPrinter<int>::printList(list);
+    // LinkedListPrinter<int>::printList(list);
     list.deleteLast();
-    LinkedListPrinter<int>::printList(list);
+    // LinkedListPrinter<int>::printList(list);
 
     return 0;
 }
